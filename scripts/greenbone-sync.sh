f60188e63b29c9ce730b28f7eac1d3c75d41330c
Catalin Bugariu <catalin@bugariu-catalin.com>
2020-09-08 16:43:47 +0100

    Update sync script

--- scripts/greenbone-sync.sh
+++ scripts/greenbone-sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-time greenbone-nvt-sync --rsync
+time greenbone-nvt-sync --rsync > /dev/null
 sleep 15
-time greenbone-certdata-sync
+time greenbone-certdata-sync > /dev/null
 sleep 15
 time greenbone-scapdata-sync > /dev/null
